662952d9c7e5ba2f29d831388395ea38b8d97699
Rahin Haque <haquerahin743@gmail.com>
2026-03-10 22:18:14 +0600

    Test

---
+++ javascript/script.js
@@ -0,0 +1,18 @@
+let isStatus =  document.querySelector("h5");
+let addFriend = document.querySelector("#add");
+let check = 0;
+
+addFriend.addEventListener('click' , function(){
+  if(check == 0){
+    isStatus.innerText = "Friends";
+    isStatus.style.color = "lightskyblue";
+    addFriend.innerText = "Remove";
+    check = 1;
+  }else if(check == 1){
+    isStatus.innerText = "Stranger";
+    isStatus.style.color = "lightcoral";
+    addFriend.innerText = "Add Friends";
+    check = 0;
+  }
+});
+
